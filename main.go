459fca93af79fe3d95ef67cf5ab79d095f0a964d
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2024-02-10 17:29:58 -0400

    new function

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 func main() {
 
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+		c.Next()
+	})
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
